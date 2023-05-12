bf735998a611fa720bd5b62519ed68b906cb99b2
isongjosiah <isongjosiah@gmail.com>
2023-05-12 19:49:42 +0100

    Setup getting input in REPL format

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include "iostream"
+#include <semaphore>
+#include <string>
 
-int main() {
-  std::cout << "Hello from cpplox!";
+int main(int argc, char *argv[]) {
+  // check input from command line
+  if (argc == 1) {
+    // handle REPL mode
+    for(;;) {
+      
+      std::cout << "> ";
+      std::string command;
+      std::getline(std::cin, command);
+      
+      if (command.empty()) {
+        std::cout << "command is empty\n";
+        break;
+      }
+      
+      // do repl here
+      std::cout << "command " << command << " not supported right now\n";
+    }
+    
+  } else {
+    char *filePath = argv[1];
+    
+    // handle source file mode
+  }
+
+  std::cout << "Thank you for using cpplox!\n";
   return 0;
 }
